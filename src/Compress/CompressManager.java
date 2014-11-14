1fea3cd8188f33af155acff4455496922706c1b5
luyisi <luyisi@155.41.32.152>
2014-11-14 18:24:20 -0500

    ll

--- src/Compress/CompressManager.java
+++ src/Compress/CompressManager.java
@@ -1,16 +1,34 @@
 package Compress;
 
+
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.ArrayList;
+
+import FileOperation.FileOperation;
 
 public class CompressManager {
 	private static CompressManager mCompressManager;
 	// checkSum, <hash, fileName>
-	private HashMap<Integer, HashMap<String, String>> mBlockMap;
-
+	//private HashMap<Integer, HashMap<String, String>> mBlockMap;
+	private HashMap<String,String> mBlockMap;
+	private int BLOCKSIZE;
+	private String inFile;
+	private ArrayList<String> curFiles=new ArrayList<String>();
+	private ArrayList<String> fileContents=new ArrayList<String>();
+	
+	 
+	
+	
+	
 	public CompressManager() {
+		//inFile=s;
+		FileOperation.readFile("");
 
 	}
-
+/**
+ * 把所有以保存的file写入map
+ */
 	public static void init() {
 		mCompressManager = new CompressManager();
 	}
@@ -27,3 +45,4 @@ public class CompressManager {
 		return null;
 	}
 }
+
